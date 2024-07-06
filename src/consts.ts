beeb0915c2331501034fcaf26bcc362433585976
tlouchao <48967399+tlouchao@users.noreply.github.com>
2024-07-05 20:20:29 -0400

    initial test dark mode button

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Tiffany Chao';
+export const SITE_DESCRIPTION = 'Tiffany Chao | Portfolio & Blog';
