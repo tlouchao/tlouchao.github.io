6c099eda797a1081840f1d47665d595ef6eaa085
tlouchao <48967399+tlouchao@users.noreply.github.com>
2024-08-03 02:00:02 -0400

    auto expand/collapse sub items if mobile screen width

--- src/scripts/stores/expand.ts
+++ src/scripts/stores/expand.ts
@@ -5,8 +5,28 @@ interface ExpandInterface {
 }
 
 export const $expand = map<ExpandInterface>(
-    {
-        navbar: false, 
-        gallery: false,
+    { 
+        navbar: false,
+        gallery: true,
     }
 )
+
+enum ExpandEvent {
+    ENABLE = "enableautoexpand",
+    DISABLE = "disableautoexpand",
+  }
+
+// ----------------------------- Media Query ------------------------------- //
+
+var mql : MediaQueryList = window.matchMedia("(max-width: 768px)");
+
+// monitor the viewport size
+// dispatch custom event on reaching media query breakpoint
+const handleMqlChange = (evtTarget : HTMLElement) => {
+    return function handleMqlChangeInner(evt : MediaQueryListEvent) {
+        let newEvt : Event = (mql.matches)? new Event(ExpandEvent.ENABLE) : new Event(ExpandEvent.DISABLE);
+        evtTarget.dispatchEvent(newEvt);
+    }
+}
+
+export default { ExpandEvent, mql, handleMqlChange };
