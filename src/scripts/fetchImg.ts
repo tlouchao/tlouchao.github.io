3b5ffb8fed1d8b66dccf3aacc38d34dae49be50d
tlouchao <48967399+tlouchao@users.noreply.github.com>
2024-08-23 00:27:27 -0400

    get valid links status code 200

--- src/scripts/fetchImg.ts
+++ src/scripts/fetchImg.ts
@@ -1,6 +1,10 @@
-import { v2 as cloudinary } from 'cloudinary';
+import { v2 as cloudinary } from 'cloudinary'
+import axios from 'axios'
+import path from 'path'
+import url from 'url'
 
 const cloudName = import.meta.env.PUBLIC_CLOUD_NAME;
+const cloudHostname = import.meta.env.PUBLIC_CLOUD_HOSTNAME;
 const cloudPrefix = import.meta.env.PUBLIC_CLOUD_PREFIX;
 const apiKey = import.meta.env.CLOUD_API_KEY;
 const apiSecret = import.meta.env.CLOUD_API_SECRET;
@@ -13,31 +17,70 @@ cloudinary.config({
     secure: true,
 });
 
+//--------------------------------Fetch Images-------------------------------//
+
+interface AssetInterface {
+    public_id: string,
+    version: number,
+    format: string,
+}
+
+interface ResponseInterface {
+    resources: AssetInterface[],
+}
+
 var fetchImg = async function(prefix : string) : Promise<string[] | void> {
-    
+
+    // Make sure that this function is executed once
     var fetchImgExecuted = false;
     if (fetchImgExecuted) return;
 
-    interface ResponseInterface {
-        resources: { public_id: string }[],
+    let validAssets : AssetInterface[] = [];
+
+    // Fetch the images given some prefix
+    const options = {prefix: prefix, type: 'upload'};
+    let assets = await cloudinary.api.resources(options)
+        .then((response : ResponseInterface) => {
+            return response['resources']?.map(r => {
+                return {public_id: r['public_id'], 
+                        version: r['version'],
+                        format: r['format']};
+            })
+        })
+        .catch((err) => {})
+
+    // Get valid images with status code 200
+    if (assets) {
+
+        let urlObj = {
+            protocol: 'https',
+            hostname: cloudHostname,
+            pathname: '',
+        };
+
+        let predicate = async(url : string) : Promise<boolean> => {
+            return axios.head(url)
+                .then((response) => response.status == 200)
+                .catch((err) => false);
+        };
+
+        // Boolean mask with predicate
+        await Promise.all(
+            assets.map(async(a) => {
+                urlObj.pathname = path.join(cloudName, 'v' + a.version.toString(), a.public_id + '.' + a.format);
+                return predicate(url.format(urlObj));
+            }))
+            .then((response : boolean[]) => {
+                validAssets = assets.filter((v, idx) => response[idx]);
+            })
+            .catch((err) => {});
     }
 
-    let options = {
-        prefix: prefix,
-        type: 'upload',
-        max_results: 50,
-    };
-    
-    let publicIds = cloudinary.api.resources(options)
-        .then((response : ResponseInterface) => {
-            return response['resources']?.map(r => r['public_id']);
-        })
-        .catch((err) => {
-            console.log(err);
-        });
-    
+    // Make sure that this function is executed once
     fetchImgExecuted = true;
-    return publicIds;
+    
+    // Return the filtered images
+    return (validAssets) ? validAssets?.map(a => a.public_id) : assets?.map(a => a.public_id);
 }
 
 export const publicIds : string[] | void = await fetchImg(cloudPrefix);
