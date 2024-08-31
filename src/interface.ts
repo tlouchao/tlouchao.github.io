614b7984a7b77f7dc7ad16199032f2883ca53da0
tlouchao <48967399+tlouchao@users.noreply.github.com>
2024-08-31 16:21:47 -0400

    sticky aside element

--- src/interface.ts
+++ src/interface.ts
@@ -2,7 +2,7 @@
 export interface PidInterface {
     public_id: string,
     tags: string[],
-}
+};
 
 // client render
 export interface GalleryItemInterface {
@@ -14,4 +14,19 @@ export interface GalleryItemProps {
     src: string, 
     label: string, 
     tags: string[],
+};
+
+// explicitly define frontmatter props for Typescript type safety? 
+// this should be auto-generated
+export interface FrontmatterProps {
+    title?: string,
+    url?: string,
+    frontmatter?: {
+        title : string,
+    },
+    headings?: {
+        depth: number,
+        text: string,
+        slug: string,
+    }[],
 };
